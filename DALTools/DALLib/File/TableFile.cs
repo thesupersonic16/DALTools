196d7c6dd03f605de861c3b0bb7596df6655ef9f
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2023-09-08 07:04:38 +1000

    Added table editing

---
+++ DALTools/DALLib/File/TableFile.cs
@@ -0,0 +1,131 @@
+﻿using DALLib.IO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALLib.File
+{
+    public class TableFile : FileBase
+    {
+        public List<TableColumn> Columns = new List<TableColumn>();
+        public List<string[]> Rows = new List<string[]>();
+
+        public override void Load(ExtendedBinaryReader reader, bool keepOpen = false)
+        {
+            int columnCount = reader.ReadInt32();
+            int rowCount = reader.ReadInt32();
+            int rowPosition = reader.ReadInt32();
+
+            Columns.Clear();
+            Rows.Clear();
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                Columns.Add(new TableColumn(
+                    (TableType)reader.ReadUInt32(), 
+                    reader.ReadStringElsewhere()));
+            }
+
+            // TODO: Is this needed?
+            reader.JumpTo(rowPosition);
+
+            for (int r = 0; r < rowCount; r++)
+            {
+                var row = new string[columnCount];
+                for (int c = 0; c < columnCount; c++)
+                {
+                    switch (Columns[c].Type)
+                    {
+                        case TableType.Int:
+                            row[c] = reader.ReadInt32().ToString(CultureInfo.CreateSpecificCulture("en-GB"));
+                            break;
+                        case TableType.String:
+                            row[c] = reader.ReadStringElsewhere();
+                            break;
+                        default:
+                            throw new NotImplementedException(Columns[c].Type.ToString());
+                    }
+                }
+                Rows.Add(row);
+            }
+        }
+
+        public override void Save(ExtendedBinaryWriter writer)
+        {
+            writer.Write(Columns.Count);
+            writer.Write(Rows.Count);
+            writer.AddOffset("rowPosition");
+
+            for (int col = 0; col < Columns.Count; col++)
+            {
+                writer.Write((uint)Columns[col].Type);
+                writer.AddOffset($"column{col}");
+            }
+
+            writer.FillInOffset("rowPosition");
+
+            for (int row = 0; row < Rows.Count; row++)
+            {
+                for (int col = 0; col < Columns.Count; col++)
+                {
+                    switch (Columns[col].Type)
+                    {
+                        case TableType.Int:
+                            writer.Write(Convert.ToInt32(Rows[row][col], CultureInfo.CreateSpecificCulture("en-GB")));
+                            break;
+                        case TableType.String:
+                            writer.AddOffset($"str{row}_{col}");
+                            break;
+                        default:
+                            throw new NotImplementedException(Columns[col].Type.ToString());
+                    }
+                }
+            }
+
+            // Write strings
+            for (int col = 0; col < Columns.Count; col++)
+            {
+                writer.FillInOffset($"column{col}");
+                writer.WriteNullTerminatedString(Columns[col].Name);
+            }
+
+            for (int row = 0; row < Rows.Count; row++)
+            {
+                for (int col = 0; col < Columns.Count; col++)
+                {
+                    if (Columns[col].Type == TableType.String)
+                    {
+                        writer.FillInOffset($"str{row}_{col}");
+                        writer.WriteNullTerminatedString(Rows[row][col]);
+                    }
+                }
+            }
+        }
+
+        public enum TableType
+        {
+            Unknown0,
+            Int,
+            Unknown2,
+            Unknown3,
+            Unknown4,
+            String,
+        }
+
+        public class TableColumn
+        {
+            public TableType Type;
+            public string Name;
+
+            public TableColumn(TableType type, string name)
+            {
+                Type = type;
+                Name = name;
+            }
+        }
+    }
+}
---
+++ DALTools/TableEditor/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using DALLib.File;
+using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TableEditor.UI;
+
+namespace TableEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TableFile Table { get; set; }
+        
+        protected List<ObservableCollection<string>> TableData { get; set; }
+        protected string FilePath;
+
+        public void LoadTable(string path)
+        {
+            Table = new TableFile();
+            Table.Load(path);
+
+            TableData = new List<ObservableCollection<string>>();
+            for (int i = 0; i < Table.Rows.Count; i++)
+            {
+                var column = new ObservableCollection<string>(Table.Rows[i]);
+                TableData.Add(column);
+            }
+
+            // TODO: Fix header not shrinking
+            Editor.ColumnHeadersSource = Table.Columns.Select((x, i) => x.Name).ToList();
+            Editor.ItemsSource = TableData;
+            FilePath = path;
+            Title = $"{App.ProgramName} - {Path.GetFileName(path)}";
+        }
+
+        public void SaveTable(string path)
+        {
+            Table.Rows.Clear();
+            foreach (var row in TableData)
+                Table.Rows.Add(row.ToArray());
+            Table.Save(path);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (App.Args.Length > 0 && File.Exists(App.Args[0]))
+                LoadTable(App.Args[0]);
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePath == null)
+            {
+                MessageBox.Show("No table is loaded.", "Save Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var shiftKey = Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
+            if (shiftKey)
+            {
+                var sfd = new SaveFileDialog
+                {
+                    Filter = "Table Files (*.bin)|*.bin",
+                    FileName = FilePath
+                };
+                if (sfd.ShowDialog() == true)
+                    SaveTable(sfd.FileName);
+            }
+            else
+                SaveTable(FilePath);
+        }
+
+        private async void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = "Table Files (*.bin)|*.bin"
+            };
+            if (ofd.ShowDialog() == true)
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    LoadTable(ofd.FileName);
+                });
+            }
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            new AboutWindow().ShowDialog();
+        }
+    }
+}
