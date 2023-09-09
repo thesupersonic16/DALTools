d1edad8e49d395f3a217aa5968c74eaa3997ddd1
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2023-09-10 00:44:47 +1000

    Workflows attempt 6

--- DALTools/PrepareRelease.bat
+++
@@ -1,9 +0,0 @@
-mkdir Release
-copy DALLib\bin\Release\DALLib.dll Release\DALLib.dll
-copy PCKTool\bin\Release\PCKTool.exe Release\PCKTool.exe
-copy STSCTool\bin\Release\STSCTool.exe Release\STSCTool.exe
-copy TEXTool\bin\Release\TEXTool.exe Release\TEXTool.exe
-copy FontEditor\bin\Release\FontEditor.exe Release\FontEditor.exe
-copy ScriptDatabaseEditor\bin\Release\ScriptDatabaseEditor.exe Release\ScriptDatabaseEditor.exe
-copy ScriptDialogueEditor\bin\Release\ScriptDialogueEditor.exe Release\ScriptDialogueEditor.exe
-copy TableEditor\bin\Release\TableEditor.exe Release\TableEditor.exe
