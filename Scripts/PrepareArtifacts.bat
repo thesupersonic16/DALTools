f9fe6d09be368134c4fd7ec3656a8dc9683d4706
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2023-09-10 01:05:35 +1000

    Workflows attempt 8

--- Scripts/PrepareArtifacts.bat
+++
@@ -1,9 +0,0 @@
-mkdir .\Artifacts
-copy .\DALTools\DALTools\DALLib\bin\%1\DALLib.dll .\Artifacts\DALLib.dll
-copy .\DALTools\DALTools\PCKTool\bin\%1\PCKTool.exe .\Artifacts\PCKTool.exe
-copy .\DALTools\DALTools\STSCTool\bin\%1\STSCTool.exe .\Artifacts\STSCTool.exe
-copy .\DALTools\DALTools\TEXTool\bin\%1\TEXTool.exe .\Artifacts\TEXTool.exe
-copy .\DALTools\DALTools\FontEditor\bin\%1\FontEditor.exe .\Artifacts\FontEditor.exe
-copy .\DALTools\DALTools\ScriptDatabaseEditor\bin\%1\ScriptDatabaseEditor.exe .\Artifacts\ScriptDatabaseEditor.exe
-copy .\DALTools\DALTools\ScriptDialogueEditor\bin\%1\ScriptDialogueEditor.exe .\Artifacts\ScriptDialogueEditor.exe
-copy .\DALTools\DALTools\TableEditor\bin\%1\TableEditor.exe .\Artifacts\TableEditor.exe
